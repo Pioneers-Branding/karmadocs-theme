0db45013074522275242930d9223e5f0c8bac847
Shivam020202 <charlieshivam70@gmail.com>
2026-06-24 20:59:37 +0530

    changes

--- front-page.php
+++ front-page.php
@@ -552,7 +552,7 @@ get_header();
                     </span>
                     <span
                         class="inline-flex items-center px-3 py-1 rounded-full text-xs font-semibold bg-blue-100 text-blue-700">
-                        <i class="fas fa-clock mr-1.5"></i> 20+ Years Experience
+                        <i class="fas fa-clock mr-1.5"></i> 30+ Years Experience
                     </span>
                 </div>
 
